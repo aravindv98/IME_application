483454b2b8f812855ebbf193865467863e849168
Aravind Venkatapathy <arsenal.aravind@gmail.com>
2023-04-09 11:52:21 -0400

    Added intensity line

--- src/view/HistogramChart.java
+++ src/view/HistogramChart.java
@@ -32,6 +32,8 @@ public class HistogramChart implements IHistogram {
   private Map<Integer, Integer> redFrequencyMap = new HashMap<>();
   private Map<Integer, Integer> greenFrequencyMap = new HashMap<>();
   private Map<Integer, Integer> blueFrequencyMap = new HashMap<>();
+
+  private Map<Integer, Integer> intensityFrequencyMap = new HashMap<>();
   private static final int BINS = 256;
 
   protected HistogramChart(Pixels properties) {
@@ -41,6 +43,7 @@ public class HistogramChart implements IHistogram {
       this.redFrequencyMap.put(i, 0);
       this.greenFrequencyMap.put(i, 0);
       this.blueFrequencyMap.put(i, 0);
+      this.intensityFrequencyMap.put(i,0);
     }
   }
 
@@ -51,27 +54,35 @@ public class HistogramChart implements IHistogram {
       int height = properties.height;
       for (int y = 0; y < height; y++) {
         for (int x = 0; x < width; x++) {
+
           String[] arr = properties.listOfPixels[x][y].split(" ");
+          int intensityValue = (Integer.parseInt(arr[0])+Integer.parseInt(arr[1])
+                  +Integer.parseInt(arr[2]))/3;
           this.redFrequencyMap.put(Integer.parseInt(arr[0]),
                   this.redFrequencyMap.get(Integer.parseInt(arr[0]))+1);
           this.greenFrequencyMap.put(Integer.parseInt(arr[1]),
                   this.greenFrequencyMap.get(Integer.parseInt(arr[1]))+1);
           this.blueFrequencyMap.put(Integer.parseInt(arr[2]),
                   this.blueFrequencyMap.get(Integer.parseInt(arr[2]))+1);
+          this.intensityFrequencyMap.put(intensityValue,
+                  this.intensityFrequencyMap.get(intensityValue)+1);
         }
       }
     final XYSeriesCollection dataset = new XYSeriesCollection( );
     final XYSeries redSeries = new XYSeries( "Red" );
     final XYSeries greenSeries = new XYSeries( "Green" );
     final XYSeries blueSeries = new XYSeries( "Blue" );
+    final XYSeries intensitySeries = new XYSeries( "Intensity" );
     for(int i = 0;i<=256;i++){
       redSeries.add(i, this.redFrequencyMap.get(i));
       greenSeries.add(i, this.greenFrequencyMap.get(i));
       blueSeries.add(i, this.blueFrequencyMap.get(i));
+      intensitySeries.add(i,this.intensityFrequencyMap.get(i));
     }
     dataset.addSeries(redSeries);
     dataset.addSeries(greenSeries);
     dataset.addSeries(blueSeries);
+    dataset.addSeries(intensitySeries);
 
     JFreeChart chart = ChartFactory.createXYLineChart("Histogram", "Pixel Value",
             "Frequency", (XYDataset) dataset, PlotOrientation.VERTICAL, true, true, false);
@@ -80,6 +91,11 @@ public class HistogramChart implements IHistogram {
     renderer.setSeriesPaint( 0 , Color.RED );
     renderer.setSeriesPaint( 1 , Color.GREEN );
     renderer.setSeriesPaint( 2 , Color.BLUE );
+    renderer.setSeriesPaint( 3 , Color.GRAY );
+    renderer.setSeriesShapesVisible(0,false);
+    renderer.setSeriesShapesVisible(1,false);
+    renderer.setSeriesShapesVisible(2,false);
+    renderer.setSeriesShapesVisible(3,false);
     plot.setRenderer(renderer);
     ChartPanel panel = new ChartPanel(chart);
     return panel;
